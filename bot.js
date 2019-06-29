5e623f0c822a3f33a00482907deb8c08b684d9cb
Alex Maddern <alex.maddern@arcitecta.com>
2019-06-29 18:22:59 +1000

    Added catch all option to bot

--- bot.js
+++ bot.js
@@ -1,18 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+console.log(`Nemesis Starting...`);
+client.login(process.env.BOT_TOKEN);
+
+
 client.on('ready', () => {
 	console.log(`Nemesis: Logged in as ${client.user.tag}!`);
 });
 
+
 client.on('message', message => {
-	if (message.content === 'Nemesis') {
+
+	if (message.content.toLowerCase() === 'nemesis') {
 		message.reply('GRRRAW!!');
+		return;
 	}
-	if (message.content === 'Nemesis eat Chote') {
-		message.reply('Mmmmmm... tasty, tasty Chote. *NOM*');
+
+	if (message.content.toLowerCase() === 'nemesis eat chote') {
+		message.reply('Mmmmmm... tasty, tasty Chote. \*NOM!\* \*NOM!\* \*NOM!\* Delicious.');
+		return;
 	}
+
+
+
+	if (message.content.toLowerCase().includes('nemesis')) {
+		message.reply('I\'m listening...');
+	}
+
 });
 
-console.log(`Nemesis Starting...`);
-client.login(process.env.BOT_TOKEN);
