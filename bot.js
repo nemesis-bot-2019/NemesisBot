89939567a2436f497e6f4782b17d848712e6f6d8
Alex Maddern <alex.maddern@arcitecta.com>
2019-06-29 18:30:32 +1000

    Added catch all option to bot

--- bot.js
+++ bot.js
@@ -5,26 +5,29 @@ console.log(`Nemesis Starting...`);
 client.login(process.env.BOT_TOKEN);
 
 
-client.on('ready', () => {
+client.on( 'ready', () => {
 	console.log(`Nemesis: Logged in as ${client.user.tag}!`);
 });
 
 
-client.on('message', message => {
+client.on( 'message', msg => {
 
-	if (message.content.toLowerCase() === 'nemesis') {
-		message.reply('GRRRAW!!');
-		return;
-	}
+	var message = msg.content.toLowerCase();
+	if ( message.startsWith('nemesis') ) {
 
-	if (message.content.toLowerCase() === 'nemesis eat chote') {
-		message.reply('Mmmmmm... tasty, tasty Chote. \*NOM!\* \*NOM!\* \*NOM!\* Delicious.');
-		return;
-	}
+		if ( message === 'nemesis' ) {
+			message.reply('GRRRAW!!');
+			return;
+		}
+
+		if (message.includes('eat chote')) {
+			message.reply('Mmmmmm... tasty, tasty Chote. \*NOM!\* \*NOM!\* \*NOM!\* Delicious.');
+			return;
+		}
+
+		
 
 
-
-	if (message.content.toLowerCase().includes('nemesis')) {
 		message.reply('I\'m listening...');
 	}
 
