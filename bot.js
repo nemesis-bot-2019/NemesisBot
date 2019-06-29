f7cec781daa02566dfbf2802207adb6dc4bf0204
Alex Maddern <alex.maddern@arcitecta.com>
2019-06-29 18:40:39 +1000

    Generic bot updates

--- bot.js
+++ bot.js
@@ -20,16 +20,28 @@ client.on( 'message', message => {
 			return;
 		}
 
+		if (text.includes('who\'s the')) {
+			message.reply('bigAPE... duh.');
+			return;
+		}
+
 		if (text.includes('eat chote')) {
 			message.reply('Mmmmmm... tasty, tasty Chote. \*NOM!\* \*NOM!\* \*NOM!\* Delicious.');
 			return;
 		}
 
-
-
+		if (text.includes('selfie')) {
+	        const attachment = new Attachment('./resources/nemesis-1.jpg');
+	        message.channel.send(`${message.author},`, attachment);
+		}
 
 		message.reply('I\'m listening...');
 	}
+
+	if ( text === 'help nemesis=' ) {
+		message.reply('Trying asking Nemesis for a selfie.');
+	}
+
 
 });
 
